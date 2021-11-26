754c41b36dde5526093367328ccb274185bfc2d2
louisledoux <19322697+louisledoux@users.noreply.github.com>
2021-11-26 15:27:05 +0100

    fix(authUtils): fix authUtils token expiration variables

--- src/utils/auth/authUtils.spec.ts
+++ src/utils/auth/authUtils.spec.ts
@@ -1,4 +1,4 @@
-import { ONE_WEEK } from '../../constants';
+import { ONE_DAY } from '../../constants';
 import { isTokenExpired } from './authUtils';
 
 // Setup Date.now() with a fixed value to test token expiration
@@ -7,16 +7,16 @@ beforeEach(() => {
 });
 
 test('Token should be expired', () => {
-  const expiresIn = ONE_WEEK;
-  const emittedAt = 1599395199000; // One week before Date.now() - 1 sec
+  const expiresIn = ONE_DAY;
+  const emittedAt = 1599913599000; // One day before Date.now() - 1 sec
   const result = isTokenExpired(expiresIn, emittedAt);
   expect(result).toBeDefined();
   expect(result).toBe(true);
 });
 
 test('Token should NOT be expired', () => {
-  const expiresIn = ONE_WEEK;
-  const emittedAt = 1599395201000; // One week before Date.now() + 1 sec
+  const expiresIn = ONE_DAY;
+  const emittedAt = 1599913601000; // One day before Date.now() + 1 sec
   const result = isTokenExpired(expiresIn, emittedAt);
   expect(result).toBeDefined();
   expect(result).toBe(false);
